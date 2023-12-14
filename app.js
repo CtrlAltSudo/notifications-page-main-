74c55537c6b842cfc0b166e2b4e0ba73a86c31dc
Damien White <damienkwhite@gmail.com>
2023-12-14 20:25:33 +0000

    alert icons and unread notification count now update on button click

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const readButton = document.getElementsByTagName("h2")[0];
 const unreadNotif = document.getElementsByClassName("unread");
+const alerts = document.getElementsByClassName("alertIcon");
+const notificationCount = document.getElementsByClassName("notificationCount")[0];
 
-readButton.addEventListener("click", (e) => {
-    e.preventDefault();
+readButton.addEventListener("click", () => {
     for(let unread of unreadNotif){
         unread.style.backgroundColor = "var(--readBackground)";
     }
+    for(let alert of alerts){
+        alert.style.visibility = "hidden"
+    }
+    notificationCount.innerText = "0";
     console.log("readme clicked!") /* To ensure button is working */
 })
